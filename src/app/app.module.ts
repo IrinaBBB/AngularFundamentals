9da2003531f6ffcdcb89b2c8a08302a5c942a497
Irina <balkarovairina@gmail.com>
2021-06-11 10:50:00 +0200

    add: event-thumbnail.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
-import {EventsListComponent} from "./events/events-list.component";
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  declarations: [
-    EventsAppComponent, EventsListComponent
-  ],
-  providers: [],
-  bootstrap: [EventsAppComponent]
+    imports: [BrowserModule],
+    declarations: [
+        EventsAppComponent,
+        EventsListComponent,
+        EventThumbnailComponent,
+    ],
+    providers: [],
+    bootstrap: [EventsAppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'ngf-events-list',
-    templateUrl: './events-list.component.html',
+    template: `
+        <div>
+            <h1>Upcoming Angular Events</h1>
+            <hr />
+            <ngf-event-thumbnail [event]="event"></ngf-event-thumbnail>
+        </div>
+    `,
 })
 export class EventsListComponent {
     event = {
